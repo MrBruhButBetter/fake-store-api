7f5f3056cadbbb688e5c846a5ddb9de8228126e9
Null_ <notanoobforsure@gmail.com>
2024-09-27 11:10:11 +0300

    Inital commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { onMounted, ref } from "vue";
+import axios from "axios"
+
+const products = ref ([])
+
+const fetchAllProduct = async () => {
+  const reponse = await axios.get("https://https://fakestoreapi.com/products")
+  products.value = reponse.data 
+
+  onMounted(async () => fetchAllProduct() )
+}
+</script>
+
+<template>
+
+  <h1>Welcome to fake e-store!</h1>
+  <div class="grid grid-cols-4 gap-4"></div>
+  <article v-for="product in products" :key="product.id">
+    <img class="w-full aspect-video object-contain" :src="product.img" alt="">
+    <h4>{{ product.title }}</h4>
+
+  </article>
+  
+</template>
